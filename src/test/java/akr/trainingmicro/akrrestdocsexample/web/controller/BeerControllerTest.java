b814230d2584daaebdf8ed1b86f01b80761715c1
arodrigues_freebalance <arodrigues@freebalance.com>
2020-06-25 13:19:06 +0100

    Documentation Generation.

--- src/test/java/akr/trainingmicro/akrrestdocsexample/web/controller/BeerControllerTest.java
+++ src/test/java/akr/trainingmicro/akrrestdocsexample/web/controller/BeerControllerTest.java
@@ -64,7 +64,7 @@ class BeerControllerTest {
         		.param("iscold","yes")
         		.accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andDo(document("/v1/beer", 
+                .andDo(document("/v1/beer-get", 
                 		pathParameters(
                 				parameterWithName("beerId").description("UUID of desired Beer to get.")
                 		),		
@@ -96,7 +96,7 @@ class BeerControllerTest {
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(beerDtoJson))
                 .andExpect(status().isCreated())
-                .andDo(document("/v1/beer", 
+                .andDo(document("/v1/beer-new", 
                 		requestFields(
                 				fields.withPath("id").ignored(),
                                 fields.withPath("version").ignored(),
