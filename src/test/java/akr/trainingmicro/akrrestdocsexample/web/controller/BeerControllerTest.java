ff9e0761c7120d6f7ee8e7d5f74c937216300baf
arodrigues_freebalance <arodrigues@freebalance.com>
2020-06-25 11:22:42 +0100

    Adding RestDocs Request Fields.

--- src/test/java/akr/trainingmicro/akrrestdocsexample/web/controller/BeerControllerTest.java
+++ src/test/java/akr/trainingmicro/akrrestdocsexample/web/controller/BeerControllerTest.java
@@ -7,6 +7,7 @@ import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuild
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.put;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
 import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
 import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
@@ -87,7 +88,19 @@ class BeerControllerTest {
         mockMvc.perform(post("/api/v1/beer/")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(beerDtoJson))
-                .andExpect(status().isCreated());
+                .andExpect(status().isCreated())
+                .andDo(document("/v1/beer", 
+                		requestFields(
+                				fieldWithPath("id").ignored(),
+                                fieldWithPath("version").ignored(),
+                                fieldWithPath("createdDate").ignored(),
+                                fieldWithPath("lastModifiedDate").ignored(),
+                                fieldWithPath("beerName").description("Beer Name"),
+                                fieldWithPath("beerStyle").description("Beer Style"),
+                                fieldWithPath("upc").description("UPC of Beer").attributes(),
+                                fieldWithPath("price").description("Price"),
+                                fieldWithPath("quantityOnHand").ignored()                				
+                )));
     }
 
     @Test
